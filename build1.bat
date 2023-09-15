8008b508deed8a5e2a2bd9c3fabfc273c0f3eecd
Paolo Minazzi <paolo.minazzi@mitrol.it>
2023-09-15 13:23:43 +0200

    miglioria

--- build1.bat
+++ build1.bat
@@ -1,6 +1,7 @@
 @ECHO OFF
 REM SET MINGW_PATH=c:\compilatori\mingw\mingw64-gcc4.5.4\bin
-SET MINGW_PATH=..\mingw64\bin
+REM SET MINGW_PATH=..\mingw64\bin
+call set_path_mingw.bat
 %MINGW_PATH%\gcc p1.c -c
 %MINGW_PATH%\gcc ui.c -c
 %MINGW_PATH%\gcc app.c -c
